1c899d0b29834365c911969cd7b39d9be156391e
Prabeshika <prabeshika14@gmail.com>
2023-12-30 02:36:05 +0100

    About page completed

--- src/Pages/AboutPage.jsx
+++ src/Pages/AboutPage.jsx
@@ -1,9 +1,77 @@
-const AboutPage = () => {
-  return (
-    <div>
-      <h1></h1>
-    </div>
-  );
-};
+import "../styles/About.css";
 
-export default AboutPage;
+function About() {
+  return (
+    <section className="section-white">
+      <div className="container">
+        <div className="row">
+          <div className="col-md-12 text-center"></div>
+
+          <div className="col-sm-6 col-md-4">
+            <div className="team-item">
+              <img
+                src="https://demo.epic-webdesign.com/tf-pacifico/v1/images/team1a.jpg"
+                className="team-img"
+                alt="pic"
+              />
+              <h3>ALIA DOSSANI</h3>
+              <div className="team-info">
+                <p>IronHack Web-Dev</p>
+                <a
+                  className="github"
+                  href="#"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  github
+                </a>
+                <a
+                  className="linkedIn"
+                  href="#"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  LinkedIn
+                </a>
+              </div>
+            </div>
+          </div>
+
+          <div className="col-sm-6 col-md-4">
+            <div className="team-item">
+              <img
+                src="src/assets/prabeshikaProfile.jpg"
+                className="team-img"
+                alt="pic"
+              />
+
+              <h3>PRABESHIKA KOIRALA</h3>
+
+              <div className="team-info">
+                <p>IronHack Web-Dev</p>
+                <a
+                  className="github"
+                  href="https://github.com/Prabeshikak?tab=repositories"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  github
+                </a>
+                <a
+                  className="linkedIn"
+                  href="#"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  LinkedIn
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default About;
