cc3cebc325211dada93e70d9bbe0bcebc421745c
Alia Dossani <dossanialia1408@gmail.com>
2024-01-06 12:05:03 +0100

    removed all filters

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -11,38 +11,6 @@ const Navbar = ({ isLoggedIn, userEmail }) => {
             <img className={classes.logo} src={logo} />
           </Link>
         </div>
-
-        <select>
-          <option value="" disabled hidden selected>
-            Work-Mode
-          </option>
-          <option value="Remote">Remote</option>
-          <option value="In-Office">In-Office</option>
-          <option value="Hybrid">Hybrid</option>
-        </select>
-        <select>
-          <option value="" disabled hidden selected>
-            Experience
-          </option>
-          <option value="Fresher">Fresher</option>
-          <option value="Junior">Junior</option>
-          <option value="Mid-level">Mid-level</option>
-          <option value="Senior">Senior</option>
-        </select>
-        <select>
-          <option value="" disabled hidden selected>
-            JobType
-          </option>
-          <option value="Full-time">Fresher</option>
-          <option value="Part-time">Junior</option>
-          <option value="Contract">Mid-level</option>
-        </select>
-        <div>
-          {/* <input type="text" placeholder="Search" />*/}
-          <button className="Button" type="button">
-            Search
-          </button>
-        </div>
       </div>
 
       <div className={classes.navElements}>
